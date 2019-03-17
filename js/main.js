d5a914bd438513daae3327a7d0bc29ad03d2d9f3
AleksanderSkubala <olek_s@wtvk.pl>
2019-03-17 22:19:08 +0100

    End of doing the adding function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //onload
 check();
-const allTodos = getData();
+let allTodos = getData();
 showData(allTodos);
 
 
@@ -23,6 +23,8 @@ add.onclick = () => {
         allTodos.push(newTodo);
         var obj = {'all': allTodos};
         push('todos', JSON.stringify(obj));
+
+        inputs[0].value, inputs[1].value, inputs[2].value = '';
         showData(getData());
     }
 };
@@ -32,9 +34,23 @@ function arrow(index) {
     div.classList.toggle('hidden');
 }
 
+function deleteEl(index) {
+    if(index===0) {
+        allTodos.slice(1);
+    } else if(index===allTodos.length-1) {
+        allTodos.slice(0, allTodos.length-2);
+    } else {
+        const arr1 = allTodos.slice(0, index);
+        const arr2 = allTodos.slice(index+1);
+        allTodos = arr1.concat(arr2);
+    }
+
+    showData(getData());
+}
+
 function showData(data) {
     var list = document.querySelector('#list');
-    // list.innerHTML = '';
+    list.innerHTML = '';
     var fragment = document.createDocumentFragment();
 
     data.forEach( (value,index) =>{
