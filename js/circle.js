63d582ef38f70fdb6b8e3087f0f85f673a30dee0
Aleksander Skubała <35501014+AleksanderSkubala@users.noreply.github.com>
2019-03-26 08:52:40 +0100

    Timer restart update via upload

--- js/circle.js
+++ js/circle.js
@@ -5,26 +5,33 @@ var countdown = countdownInitial;
 var play = document.querySelector('#buttonStart');
 var reset = document.querySelector('#buttonReset');
 var circle = document.querySelector('#circle');
+var interval;
+var min, sec;
+
 circle.style.webkitAnimationPlayState = "paused";
 
+function pushText(time) {
+	min = Math.floor(time/60);
+    sec = time%60;
+    countdownNumberEl.textContent = `${min}:${sec}`;
+}
+
 play.onclick = ()=>{
+  circle = document.querySelector('#circle');
   if(circle.style.webkitAnimationPlayState === "paused") {
     clearInterval(interval);
     countdown = countdownInitial;
     circle.style.webkitAnimationPlayState = "running";
 
-    var min = Math.round(countdown/60);
-    var sec = countdown%60;
-    countdownNumberEl.textContent = `${min}:${sec}`;
+    pushText(countdown);
 
-    var interval = setInterval(function() {
+    interval = setInterval(function() {
       if(countdown <= 1) clearInterval(interval);
 
       countdown = --countdown <= 0 ? countdownInitial : countdown;
-
-      min = Math.floor(countdown/60);
-      sec = countdown%60;
-      countdownNumberEl.textContent = `${min}:${sec}`;
+	  
+	  pushText(countdown);
+      
     }, 1000);
   }
 };
@@ -32,7 +39,13 @@ play.onclick = ()=>{
 reset.onclick = ()=>{
   if(circle.style.webkitAnimationPlayState === "running") {
     circle.style.webkitAnimationPlayState = "paused";
+	
     clearInterval(interval);
+	countdown = countdownInitial;
+	pushText(countdown);
+	
+	var newone = circle.cloneNode(true);
+	circle.parentNode.replaceChild(newone, circle);
   }
 };
 
