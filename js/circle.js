bbbb2ed3a17409913cfe6901c74873d8849e7fd9
Aleksander Skubała <35501014+AleksanderSkubala@users.noreply.github.com>
2019-03-28 11:42:33 +0100

    Change resetFun

--- js/circle.js
+++ js/circle.js
@@ -41,7 +41,6 @@ play.onclick = ()=>{
 
     interval = setInterval(function() {
       if(countdown <= 1) {
-	      clearInterval(interval);
 	      resetFun();
 	      var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
               audio.play();
