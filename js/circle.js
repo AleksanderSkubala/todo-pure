2adb8ea804a2b664d55a10d77fd4099a18aebc46
AleksanderSkubala <olek_s@wtvk.pl>
2019-03-29 22:32:17 +0100

    Last(I hope) timer debbug

--- js/circle.js
+++ js/circle.js
@@ -1,5 +1,5 @@
 var countdownNumberEl = document.getElementById('countdown-number');
-var countdownInitial = 15;
+var countdownInitial = 25*60;
 var countdown = countdownInitial;
 
 var play = document.querySelector('#buttonStart');
@@ -13,20 +13,20 @@ circle.style.webkitAnimationPlayState = "paused";
 
 function pushText(time) {
 	min = Math.floor(time/60);
-    sec = time%60;
-    countdownNumberEl.textContent = `${min}:${sec}`;
+  sec = time%60;
+  countdownNumberEl.textContent = `${min}:${sec}`;
 }
 
 function resetFun(){
   if(circle.style.webkitAnimationPlayState === "running") {
     circle.style.webkitAnimationPlayState = "paused";
-	
+
     clearInterval(interval);
-	countdown = countdownInitial;
-	pushText(countdownInitial);
-	
-	var newone = circle.cloneNode(true);
-	circle.parentNode.replaceChild(newone, circle);
+    countdown = countdownInitial;
+    pushText(countdownInitial);
+
+    var newone = circle.cloneNode(true);
+    circle.parentNode.replaceChild(newone, circle);
   }
 }
 
@@ -41,16 +41,18 @@ play.onclick = ()=>{
 
     interval = setInterval(function() {
       if(countdown <= 1) {
-	      resetFun();
-	      var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
-              audio.play();
+        resetFun();
+        countdown++;
+	      var audio = new Audio('./other/rooster.mp3'); //https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3
+        audio.play();
       }
 
       countdown = --countdown <= 0 ? countdownInitial : countdown;
-	  
-	  pushText(countdown);
-      
+
+      pushText(countdown);
+
     }, 1000);
+    pushText(countdownInitial);
   }
 };
 
