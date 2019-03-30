27ef2983008e2f65aebfabab9822a14486c96d92
AleksanderSkubala <olek_s@wtvk.pl>
2019-03-30 09:56:42 +0100

    add branch with mobile version

---
+++ mobile/colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+    white: 'white',
+    black: '#111111',
+    neutral: '#0074D9',
+    aqua: '#7FDBFF',
+    teal: '#39CCCC',
+    red: '#FF4136',
+    orange: '#FF851B',
+    primary: '#2ECC40',
+    silver: '#DDDDDD',
+    gray: '#AAAAAA',
+
+    border: 'rgba(0,0,0, .19)',
+    boxShadow: 'rgba(0,0,0, .25)',
+    boxShadowOther: 'rgba(255, 133, 27, .25)',
+}
